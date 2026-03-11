5c84707900b183beb8663bb9544e5e90d3de860e
Harcharanpreet Singh <116562226+singhHarcharan@users.noreply.github.com>
2026-03-11 14:30:42 +0530

    Update email-templates.ts

--- app/utils/email-templates.ts
+++ app/utils/email-templates.ts
@@ -1,5 +1,5 @@
 // Template for frontend interview guidance requests
-export const APPLICATION_TEMPLATE = `Subject: Quick guidance on First round interview at [Company Name]
+export const APPLICATION_TEMPLATE = `Subject: Quick guidance on First round of interview at [Company Name]
 
 Hi [Hiring Manager's Name],
 
